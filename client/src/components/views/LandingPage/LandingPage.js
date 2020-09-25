37b71be62d03ac6bfc489f53d53b09d05062fe4c
boorooksus <boorooksus@gmail.com>
2020-09-25 20:59:47 +0900

    L22 CORS 이슈, Proxy 설정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import axios, { useEffect } from 'axios';
+import React, { useEffect } from 'react'
+import axios from 'axios';
 
 function LandingPage() {
 
     useEffect(() => {
-            axios.get('http://localhost:5000/api/hello')
-            .then(response => console.log(response.data))
-        }, []);
+            axios.get('/api/hello')
+            .then(response => console.log(response))
+        }, [])
 
     return (
         <div>
