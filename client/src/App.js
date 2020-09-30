423aec727c56ceacee2b11aef34029d1565c2729
boorooksus <boorooksus@gmail.com>
2020-09-30 15:26:55 +0900

    L33, L34 인증체크

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import {
 import LandingPage from './components/views/LandingPage/LandingPage'
 import LoginPage from './components/views/LoginPage/LoginPage'
 import RegisterPage from './components/views/RegisterPage/RegisterPage'
+import Auth from './hoc/auth'
 
 function App() {
   return (
@@ -19,9 +20,9 @@ function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/login" component={LoginPage}/>
-          <Route path="/register" component={RegisterPage} />
+          <Route exact path="/" component={Auth(LandingPage, null, true)} />
+          <Route path="/login" component={Auth(LoginPage, false)}/>
+          <Route path="/register" component={Auth(RegisterPage, false)} />
         </Switch>
       </div>
     </Router>
